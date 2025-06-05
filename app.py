0d79e67943c9067caf8e922be2291de98ef1cdd5
Remy Gakwaya <32406787+labKnowledge@users.noreply.github.com>
2025-06-05 10:38:11 -0600

    returned data with   original file

--- app.py
+++ app.py
@@ -64,6 +64,8 @@ def create_app():
                 "job_id": job_id,
                 "response": response[0] if response[2] == 200 else None,
                 "message": "success" if response[2] == 200 else response[0],
+                "originalFile": response[3] if len(response) > 3 else "",
+                "subtitlefile": response[4] if len(response) > 3 else "",
                 "pid": pid,
                 "queue_id": queue_id,
                 "run_time": round(run_time, 3),
